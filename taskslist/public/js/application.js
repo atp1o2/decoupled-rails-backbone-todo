ed266ddd679cc8d85a07560fc2dfb2702c1e5fdb
Andrew Pham <email": null>
2015-08-23 23:51:19 -0700

    working

--- taskslist/public/js/application.js
+++ taskslist/public/js/application.js
@@ -4,7 +4,6 @@
     Views: {}
   }
 
-
 //-------------------------------
 // MODEL
 //-------------------------------
@@ -15,8 +14,6 @@ App.Models.Task = Backbone.Model.extend({
   }
 });
 
-
-
 //-------------------------------
 // COLLECTION
 //-------------------------------
@@ -25,44 +22,37 @@ App.Collections.Tasks = Backbone.Collection.extend({
   model: App.Models.Task
 })
 
-
 //-------------------------------
 // VIEW
 //-------------------------------
 
 // singular item
-App.Views.TaskView = Backbone.View.extend({
+var TaskView = Backbone.View.extend({
+  // tagName is a special method that works with template
   tagName: 'li',
   template: _.template("<%= content %>"),
-
   initialize: function(opts){
-    return this.listView = opts.listView;
+    return this.itemView = opts.itemView;
   },
-
   render: function(){
     this.$el.html(this.template(this.model.attributes));
     return this;
   }
 })
 
-
 // all items
 App.Views.AllView = Backbone.View.extend({
   el: '#target',
-
   initialize: function(){
     this.listenTo(this.collection, 'sync', this.render);
   },
 
   render: function(){
     var col = this.collection.models;
-
     for(var i = 0; i <= col.length; i++) {
       var model = col[i];
-
-      var itemView = new App.Views.TaskView({ model: model });
-
-      this.$el.append(itemView.render().el);
+      var taskView = new TaskView({ model: model });
+      this.$el.append(taskView.render().el);
     }
   }
 })
@@ -80,40 +70,3 @@ $(document).ready(function() {
   return App.tasks.fetch();
 });
 
-
-
-
-// var AppView = Backbone.View.extend({
-//   el: $("#target"),
-//   // _.template(templateString, [data], [settings])
-//   // templateString is where you can insert data
-//   // can use <%=  %> to insert js code
-//   template: _.template('<h3>Hello, <%= who %></h3>'),
-
-//   initialize: function(){
-//     this.render();
-//   },
-
-//   render: function(){
-//     $(this.el).html(this.template({who: 'Andrew'}));
-//   }
-// })
-
-
-
-
-//-------------------------------
-// ROUTES
-//-------------------------------
-
-
-
-
-
-
-
-
-
-  // use ajax to call on the json data
-  // front end should be talking to the server and pull the data
-  // cross domain access allow in models
